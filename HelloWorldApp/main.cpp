34b3f2b4b76fa5c56c4fc4274a425b2caea7f04c
Chad Bentz <1760475+felickz@users.noreply.github.com>
2025-11-25 15:28:10 -0500

    Add buffer overflow vulnerability demo

---
+++ HelloWorldApp/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    const char *message = "Hello";
+    char password[32];
+    char buffer[256];
+    
+    // This will overflow - writing 256 bytes when message is only 6 bytes
+    // It will write past buffer into password and beyond, corrupting the stack
+    memcpy(buffer, message, 256);
+    
+    // Make it crash more dramatically by writing way beyond buffer bounds
+    char small_buffer[8];
+    char large_data[1024];
+    memset(large_data, 'A', sizeof(large_data));
+    
+    // This will definitely overflow and corrupt stack/heap
+    memcpy(small_buffer, large_data, 1024);
+    
+    // Try to use the corrupted buffer to trigger immediate crash
+    std::cout << "If you see this, trying to access corrupted memory..." << std::endl;
+    std::cout << small_buffer[2000] << std::endl;  // Access far beyond bounds
+    
+    return 0;
+}
